4fb2441748fd77275b9d496aa6499633e4fb3160
Luis Martinez <luismach@unisabana.edu.co>
2026-03-17 19:19:34 -0500

    lab5 all try 12

--- utils_calc.py
+++ utils_calc.py
@@ -12,22 +12,26 @@ def multiply(num1, num2):
     return mult
 
 def divide(num1, num2):
-    div = (num1 / num2)
+
     if num2 == 0:
         div = "Error: Division by zero is not allowed."
+    else:
+         div = (num1 / num2)
     return div
 def exponent(base, exp):
     exp = base ** exp
     return exp
 def modulo(num1, num2):
-    res = num1 % num2
     if num2 == 0:
         res = "Error: Modulo by zero is not allowed."
+    else:
+        res = num1 % num2
     return res
 def floor_divide(num1, num2):
-    floor = math.floor(num1/num2)
     if num2 == 0:
         floor = "Error: Division by zero is not allowed."
+    else:
+        floor = math.floor(num1/num2)
     return floor
 def absolute(num1):
     absol = math.fabs(num1)
